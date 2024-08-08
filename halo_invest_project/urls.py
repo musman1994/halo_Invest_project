bb3a5b18a6f4e881dc53783fda904e44ada4eb81
musman1994 <mu.usmankhan1994@gmail.com>
2024-08-08 16:44:49 +0000

    Removed Extra Code

--- halo_invest_project/urls.py
+++ halo_invest_project/urls.py
@@ -26,7 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('trading.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
--- trading/urls.py
+++ trading/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('orders/', OrderListCreateView.as_view(), name='order_list_create'),
     path('user_stocks/<int:stock_id>/', UserStockValueView.as_view(), name='user_stock_value'),
     path('place_trades_in_bulk/', PlaceTradesInBulk.as_view(), name='place_trades_in_bulk'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
 ]
